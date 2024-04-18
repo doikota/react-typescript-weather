8f7322fb6137e5f1135ecb6f4c665d3241f5ec15
doikota <doikota@gmail.com>
2024-04-19 07:58:24 +0900

    Hello Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Title from './components/Title'
+
 const App = () => {
   return (
     <div> 
+      <Title/>
       <h1 className="test">こんにちはReact</h1>
-      <h3>さようならReact</h3>
+      <h3>よろしくReact</h3>
     </div>
   )
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1>React 世界の天気</h1>
+  )
+}
+
+export default Title
