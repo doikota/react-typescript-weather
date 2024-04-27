06384595ccffb719acfda23615731fd956686a37
doikota <doikota@gmail.com>
2024-04-27 19:06:28 +0900

    Added Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Title from './components/Title'
+import Form from './components/Form'
 
 const App = () => {
   return (
     <div> 
       <Title/>
-      <h1 className="test">こんにちはReact</h1>
-      <h3>よろしくReact</h3>
+      <Form/>
     </div>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+const Form = () => {
+  return (
+    <form>
+      <input type="text" name="city" placeholder="都市名"></input>
+      <button type="submit">天気を取得</button>
+    </form>
+  )
+}
+
+export default Form
