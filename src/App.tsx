770e7f4e8a50de2c53447b22814dbf89464807be
doikota <doikota@gmail.com>
2024-04-29 08:08:08 +0900

    Set API Call result on state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+// Reactでは画面に入力されたデータはstateと呼ばれる一時保管場所に格納し手利用する
+import { useState } from "react"
+
 import Title from './components/Title'
 import Form from './components/Form'
 
-// Reactでは画面に入力されたデータはstateと呼ばれる一時保管場所に格納し手利用する
-import { useState } from "react"
+// API呼び出しの結果を格納するオブジェクトの型
+type ResultState = {
+  country : string
+  cityName : string
+  temperature : string
+  conditionText : string
+  icon : string
+}
 
 const App = () => {
   // cityはstate, setCityはstateのデータを操作する仕組み
   // TypeScriptの型指定である<string>を用いてsetCity()の引数を文字列に限定している
   const [city, setCity] = useState<string>()
+
+  // API呼び出しの結果を格納するオブジェクト
+  const [result, setResult] = useState<ResultState>({
+    country : "",
+    cityName : "",
+    temperature : "",
+    conditionText : "",
+    icon : ""
+  })
 
   // APIを呼び出すための関数
   const getWeather = (e:any) => {
@@ -15,7 +33,15 @@ const App = () => {
     // fetchはJavaScriptでAPIを呼び出すためのメソッド　thenで受け取った結果をvoid型の関数に引数resとして渡す
     fetch("https://api.weatherapi.com/v1/current.json?key=60cc6fda7b27421a92e71655242804&q=London&aqi=no")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => { setResult ({
+          country : data.location.country,
+          cityName : data.location.name,
+          temperature : data.current.temp_c,
+          conditionText : data.current.condition.text,
+          icon : data.current.condition.icon
+        })
+      })
+      .then(a => console.log(result))
   }
 
   // Reactでは重要な設計コンセプトとして、Top-Down Data Flowを採用しており、
