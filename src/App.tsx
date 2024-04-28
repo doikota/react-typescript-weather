d143d18948071e0e1bd2ae7261a57181fb11f867
doikota <doikota@gmail.com>
2024-04-29 00:09:19 +0900

    Apply Top-Down Data Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import Title from './components/Title'
 import Form from './components/Form'
 
+// Reactでは画面に入力されたデータはstateと呼ばれる一時保管場所に格納し手利用する
+import { useState } from "react"
+
 const App = () => {
+  // cityはstate, setCityはstateのデータを操作する仕組み
+  // TypeScriptの型指定である<string>を用いてsetCity()の引数を文字列に限定している
+  const [city, setCity] = useState<string>()
+
+  // APIを呼び出すための関数
+  const getWeather = (e:any) => {
+    e.preventDefault()
+    // fetchはJavaScriptでAPIを呼び出すためのメソッド　thenで受け取った結果をvoid型の関数に引数resとして渡す
+    fetch("https://api.weatherapi.com/v1/current.json?key=60cc6fda7b27421a92e71655242804&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+  // Reactでは重要な設計コンセプトとして、Top-Down Data Flowを採用しており、
+  // これはデータの流れは一方通行、または上位から下位に向かうようにすることで、
+  // Appで定義したsetCityやgetWeatherをFormタグの属性の様に下位のコンポーネントに渡す
+  // 
   return (
     <div> 
       <Title/>
-      <Form/>
+      <Form setCity={setCity} getWeather={getWeather}/>
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,10 @@
-// Reactでは画面に入力されたデータはstateと呼ばれる一時保管場所に格納し手利用する
-import { useState } from "react"
+type FormProps = {
+  setCity : React.Dispatch<React.SetStateAction<string | undefined>>
+  getWeather : (e: any) => void
+}
 
-const Form = () => {
-
-  // cityはstate, setCityはstateのデータを操作する仕組み
-  // TypeScriptの型指定である<string>を用いてsetCity()の引数を文字列に限定している
-  const [city, setCity] = useState<string>()
-
-  // APIを呼び出すための関数
-  const getWeather = (e:any) => {
-    e.preventDefault()
-    // fetchはJavaScriptでAPIを呼び出すためのメソッド　thenで受け取った結果をvoid型の関数に引数resとして渡す
-    fetch("https://api.weatherapi.com/v1/current.json?key=60cc6fda7b27421a92e71655242804&q=London&aqi=no")
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }
+// Appとのデータの受け渡しはpropsを通じて行う
+const Form = (props : FormProps) => {
 
   // onChangeでテキストボックスの値を文字列としてsetCityを通じてcityに渡す
   return (
@@ -22,9 +12,9 @@ const Form = () => {
       <input type="text" 
               name="city" 
               placeholder="都市名" 
-              onChange={e => setCity(e.target.value)}>
+              onChange={e => props.setCity(e.target.value)}>
       </input>
-      <button type="submit" onClick={getWeather}>天気を取得</button>
+      <button type="submit" onClick={props.getWeather}>天気を取得</button>
       {/* return の中でJavaScriptのコードを書くときは{}で囲む */}
     </form>
   )
