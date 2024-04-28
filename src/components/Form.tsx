159ec3975e54feb1085ebd5a5a61bf44a56bc7eb
doikota <doikota@gmail.com>
2024-04-28 09:05:39 +0900

    Use state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,21 @@
+// Reactでは画面に入力されたデータはstateと呼ばれる一時保管場所に格納し手利用する
+import { useState } from "react"
+
 const Form = () => {
-  return (
+
+  // cityはstate, setCityはstateのデータを操作する仕組み
+  const [city, setCity] = useState("")
+    // onChangeでテキストボックスの値をsetCityを通じてcityに渡す
+    return (
     <form>
-      <input type="text" name="city" placeholder="都市名"></input>
+      <input type="text" 
+              name="city" 
+              placeholder="都市名" 
+              onChange={event => setCity(event.target.value)}>
+      </input>
       <button type="submit">天気を取得</button>
+      {city}{city}
+      {/* return の中でJavaScriptのコードを書くときは{}で囲む */}
     </form>
   )
 }
