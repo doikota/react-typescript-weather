55c9f0db786058eab2e69676fb8db35a620320bb
doikota <doikota@gmail.com>
2024-05-05 16:15:07 +0900

    First Jest test with ts-jest

---
+++ test/Weather.test.ts
@@ -0,0 +1,12 @@
+import Weather from '../src/components/Weather';
+
+test('Weather', async () => {
+  // 期待される戻り値を定義
+  const expected = 'Japan';
+
+  // Weather関数を呼び出す
+  const actual = await Weather('Yokohama');
+
+  // 期待される戻り値と実際の戻り値を比較するアサーションを行う
+  expect(actual.country).toEqual(expected);
+});
